c3c57b68700eda2ed03cbb29a0ba16e831806117
Bruno Martins <brunodefatimoms@gmail.coom>
2025-01-18 14:06:57 -0300

    Criando a classe Main e implementando o menu inicial.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int opc;
+
+        do {
+            System.out.println("\n== MENU INICIAL ==");
+            System.out.println(" 0 - Sair.");
+            System.out.print("Escolha um opção: ");
+            opc = input.nextInt();
+
+            switch (opc){
+                case 0: 
+                    System.out.println("Saindo.");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        } while (opc != 0);
     }
 }
